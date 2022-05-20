d581a7a9cce1169ce54c9b61be7defa684b0771e
[bonface221] <[mainaboniface221@gmail.com]>
2022-05-20 03:14:36 +0300

    deployment to heroku

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from . import db,login_manager
 from flask_login import UserMixin,current_user
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask import current_app 
+import os,secrets
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
@@ -8,7 +10,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(255),unique = True,nullable = False)
     email  = db.Column(db.String(255),unique = True,nullable = False)
     secure_password = db.Column(db.String(255),nullable = False)
-    claims = db.relationship('Claim', backref='user', lazy='dynamic')
+    
     
 
     @property
@@ -36,8 +38,12 @@ class User(UserMixin, db.Model):
 class Postpet(db.Model):
     __tablename__ = 'postpets'
     id = db.Column(db.Integer, primary_key = True)
-    pic_path=db.Column(db.String(255))
-    
+    pic_path=db.Column(db.String(255),unique = True,nullable = False)
+    name=db.Column(db.String(255),nullable = False)
+    age=db.Column(db.String(255),nullable = False)
+    color =db.Column(db.String(255),nullable = False)
+
+
     def save_p(self):
         db.session.add(self)
         db.session.commit()
@@ -50,3 +56,13 @@ class Postpet(db.Model):
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
+
+def upload_img(post_img):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(post_img.filename)
+    picture_filename = random_hex + f_ext
+    picture_path = os.path.join(
+        current_app.root_path, "static/photos", picture_filename
+    )
+    post_img.save(picture_path)
+    return picture_filename
--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-  SQLALCHEMY_TRACK_MODIFICATIONS=True
+  SQLALCHEMY_TRACK_MODIFICATIONS=False
   SECRET_KEY = os.environ.get('SECRET_KEY')
   UPLOADED_PHOTOS_DEST ='app/static/photos'
   MAIL_SERVER = 'smtp.googlemail.com'
@@ -13,10 +13,12 @@ class Config:
   
   
 class ProdConfig(Config):
-  SQLALCHEMY_DATABASE_URI =os.environ.get('DATABASE_URL')
+  SQLALCHEMY_DATABASE_URI = 'postgresql://keqquleglptiww:4e8db2a70c9ad2d1fc08b9ab11f82dcbb02a3c4bb1d30451de9479bcdc97ab07@ec2-34-201-95-176.compute-1.amazonaws.com:5432/d6ev25njuahra7'
 
 class DevConfig(Config):
-  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+  #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+  SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:oa2exWako@localhost/pets'
+
 
   DEBUG = True
 
